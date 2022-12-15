98cc440d1966484d7ce11f48ec53b05138b3bfc0
POLISHKITEAA <104615025+POLISHKITEAA@users.noreply.github.com>
2022-12-15 21:04:20 +0200

    Add files via upload

---
+++ task3.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        branch branch = new branch("Листик");
+
+        branch.showName();
+        branch.fly();
+        branch.sing();
+        branch.laysAnEgg();
+        branch.hatchingChicks();
+    }
+}
+class Listt{
+    String kind;
+    public Listt(String kind){
+        this.kind = kind;
+    }
+    public void showName (){
+        System.out.println("Часть дерева: " + kind );
+    }
+}
+class branch extends Listt {
+    public branch(String name) {
+        super(name);
+    }
+    public void showName(){
+        System.out.println("Часть дерева: " + kind);
+    }
+    public void fly(){
+        System.out.println(kind + " цветет");
+    }
+    public void sing(){
+        System.out.println(kind + " опадает");
+    }
+    public void laysAnEgg(){
+        System.out.println(kind + " покрывается иниеем ");
+    }
+    public void hatchingChicks(){
+        System.out.println(kind + " Желнеет");
+    }
+}
